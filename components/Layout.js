7833b5ddce78e36f54c156ba901b856720edf58d
SCOTTGeek-dev <ham.youness@gmail.com>
2024-09-16 01:18:45 +0100

    portfolio setup configured successfully by installing next and react

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,22 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// fonts
+import {Sora} from '@next/font/google'
+
+// font settings 
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700','800'],
+});
+
+//components//
+import Nav from '../components/Nav'
+import Nav from '../components/Header'
+import Nav from '../components/'
+
+const Layout = ({children}) => {
+  return <div>
+    <Nav />
+  </div>;
 };
 
 export default Layout;
